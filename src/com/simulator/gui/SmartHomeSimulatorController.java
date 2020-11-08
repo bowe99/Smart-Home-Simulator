0d66a27193fb45bdfc657f443ff104ce01aab4f3
bowe99 <kevinbowe1999@gmail.com>
2020-11-07 23:12:22 -0500

    #14 Add method to add and remove lights from security module

--- src/com/simulator/gui/SmartHomeSimulatorController.java
+++ src/com/simulator/gui/SmartHomeSimulatorController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -33,8 +34,10 @@ import java.util.Date;
 public class SmartHomeSimulatorController {
 
     @FXML private ToggleButton simulationToggle;
+    @FXML private ToggleButton awayMode;
     @FXML private Button editButton;
     @FXML private Button roomsControlPanelButton;
+    @FXML private Button saveSecurity;
     @FXML private Label displayTemp;
     @FXML private Label displayDate;
     @FXML private Label userProfile;
@@ -42,6 +45,9 @@ public class SmartHomeSimulatorController {
     @FXML private Label displayTime;
     @FXML private ListView allLightsListView;
     @FXML private ListView selectedLightsListView;
+    @FXML private TextField startTimeSecurity;
+    @FXML private TextField endTimeSecurity;
+    @FXML private TextField motionDetectedTimeSecurity;
 
     private House house;
     private SimulationParameters simulation;
@@ -120,6 +126,32 @@ public class SmartHomeSimulatorController {
     }
 
     @FXML
+    void addSecurityLight(){
+
+        Object selectedItem = this.allLightsListView.getSelectionModel().getSelectedItem();
+        this.selectedLightsListView.getItems().add((String) selectedItem);
+        this.allLightsListView.getItems().remove(selectedItem);
+
+        System.out.println((String) selectedItem);
+
+        Light selectedLight = this.house.getLightByName((String) selectedItem);
+        this.securityModule.addLight(selectedLight);
+    }
+    
+    @FXML
+    void removeSecurityLight(){
+
+        Object selectedItem = this.selectedLightsListView.getSelectionModel().getSelectedItem();
+        this.allLightsListView.getItems().add((String) selectedItem);
+        this.selectedLightsListView.getItems().remove(selectedItem);
+
+        System.out.println((String) selectedItem);
+
+        Light selectedLight = this.house.getLightByName((String) selectedItem);
+        this.securityModule.removeLight(selectedLight);
+    }
+
+    @FXML
     void openRoomControls(MouseEvent event){
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("RoomControls.fxml"));
@@ -165,6 +197,30 @@ public class SmartHomeSimulatorController {
         String hours = String.format("%02d", time/60);
         String mins = String.format("%02d", time%60);
         this.displayTime.setText(hours + ":" + mins);
+    }
+
+    @FXML
+    private void saveSecuritySettings(){
+        String startTimeInput = this.startTimeSecurity.getText();
+        String endTimeInput = this.endTimeSecurity.getText();
+        String motionDetectedTimeInput = this.motionDetectedTimeSecurity.getText();
+        try {
+            int startTime = Integer.parseInt(startTimeInput);
+            int endTime = Integer.parseInt(endTimeInput);
+            int motionDetectedTime = Integer.parseInt(motionDetectedTimeInput);
+
+            if (startTime > 1440 || startTime < 0 || endTime < 0 || endTime > 1440){
+                throw new Exception("Time is not in the correct range");
+            }
+
+            this.securityModule.saveSettings(startTime, endTime, motionDetectedTime);
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setHeaderText("Invalid input");
+            alert.setContentText("No information was saved! Please try again.");
+            alert.showAndWait();
+        }
     }
 
     @FXML
